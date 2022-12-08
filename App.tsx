b7bf8fae706745b6552c45815e18b0c94c3775db
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2022-12-08 14:10:39 +0100

    feat: re add navbar style deleted by @TristanSch1

--- App.tsx
+++ App.tsx
@@ -8,11 +8,11 @@
  * @format
  */
 
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 
-import React, {useEffect, useState} from 'react';
-import {StatusBar, StyleSheet} from 'react-native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React, { useEffect, useState } from 'react';
+import { StatusBar, StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomePage from './components/HomePage';
 import HistoryPage from './components/HistoryPage';
 import {
@@ -20,13 +20,13 @@ import {
   faHouse,
   faUser,
 } from '@fortawesome/free-solid-svg-icons';
-import {Session} from '@supabase/supabase-js';
-import {supabase} from './lib/supabase';
-import {COLORS} from './utils/globalStyle';
+import { Session } from '@supabase/supabase-js';
+import { supabase } from './lib/supabase';
+import { COLORS } from './utils/globalStyle';
 import LoginPage from './components/LoginPage';
 import ProfilePage from './components/ProfilePage';
-import {SessionProvider} from './utils/auth/SessionContext';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import { SessionProvider } from './utils/auth/SessionContext';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 
 const Tab = createBottomTabNavigator();
 
@@ -34,7 +34,7 @@ const App = () => {
   const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
-    supabase.auth.getSession().then(({data: {session: _session}}) => {
+    supabase.auth.getSession().then(({ data: { session: _session } }) => {
       setSession(_session);
     });
 
@@ -48,13 +48,17 @@ const App = () => {
       <StatusBar />
       {session ? (
         <SessionProvider value={session}>
-          <Tab.Navigator initialRouteName={'Home'}>
+          <Tab.Navigator initialRouteName={'Home'} screenOptions={{
+            tabBarStyle: styles.tabBarStyle,
+            tabBarLabelStyle: styles.tabBarLabelStyle,
+          }}>
             <Tab.Screen
               name={'homePage'}
               component={HomePage}
               options={{
                 title: 'Home',
-                tabBarIcon: () => <FontAwesomeIcon icon={faHouse} />,
+                tabBarIcon: () => <FontAwesomeIcon icon={faHouse} style={{ color: COLORS.neutral[100] }} size={25} />,
+                headerShown: false,
               }}
             />
             <Tab.Screen
@@ -62,7 +66,8 @@ const App = () => {
               component={HistoryPage}
               options={{
                 title: 'History',
-                tabBarIcon: () => <FontAwesomeIcon icon={faClockRotateLeft} />,
+                tabBarIcon: () => <FontAwesomeIcon icon={faClockRotateLeft} style={{ color: COLORS.neutral[100] }} size={25} />,
+                headerShown: false,
               }}
             />
             <Tab.Screen
@@ -70,7 +75,7 @@ const App = () => {
               component={ProfilePage}
               options={{
                 title: 'Profile',
-                tabBarIcon: () => <FontAwesomeIcon icon={faUser} />,
+                tabBarIcon: () => <FontAwesomeIcon icon={faUser} style={{ color: COLORS.neutral[100] }} size={25} />,
               }}
             />
           </Tab.Navigator>
